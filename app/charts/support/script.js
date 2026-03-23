afadf1fbc7908afe381dab58eafe8ea7e6cf1b73
Melissa <hellofucking@icloud.com>
2026-03-23 21:30:31 +0300

    Delete app/charts/support/script.js

--- app/charts/support/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.faq-question').forEach(button => {
-    button.addEventListener('click', () => {
-        const answer = button.nextElementSibling;
-        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
-    });
-});
